c24f07893785db0c1490968812c443dbb135fc77
shaikkareem <shaikkareem1728@gmail.com>
2023-08-29 15:22:21 +0530

    second

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " this is bussiness!" );
+        System.out.println( " this is bussinesS!" );
     }
 }
